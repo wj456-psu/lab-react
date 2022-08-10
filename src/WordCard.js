fb20d396e57134e47cc149897be433ca6529ea27
WisawawitJ <autoza594@gmail.com>
2022-08-11 05:06:07 +0700

    finalize part 7

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import CharacterCard from "./CharacterCard";
 import _ from 'lodash';
-
+const RefreshPage = () => {
+    window.location.reload()
+}
+const RefreshButton = () => {
+    return (
+        <button className="button-24" onClick={RefreshPage}>Restart!</button>
+    )
+}
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
@@ -41,13 +48,35 @@ export default function WordCard(props) {
         console.log(`${c} has been deactivated.`)
         setState({...state, guess: state.guess.slice(0, -1)})
     }
-    const text = `Total Attempt: ${state.attempt}`
+
+    const hintText = (given_word) => {
+        let word = given_word
+        let chars = word.split('')
+        let limChars
+        let h
+        if(state.attempt > 6){
+            limChars = chars.slice(0, -2)
+            h = limChars.join('')
+        }
+        else if(state.attempt > 4){
+            limChars = chars.slice(0, -3)
+            h = limChars.join('')
+        }
+        else if(state.attempt > 2){
+            limChars = chars.slice(0, -4)
+            h = limChars.join('')
+        }
+        return h
+    }
+    const text = `Attempt: ${state.attempt}`
     return (
         <div>
             {
                 state.chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} deActivationHandler={deActivationHandler} attempt={state.attempt}/>)
             }
-            <h2 className="centerText">{text}<br/>{state.completed ? 'YAY!':''}</h2>
+            <h2 className="centerText">{text}<br/>{state.completed ? 'YAY! The answer is ' + state.word :''}</h2>
+            {!state.completed ? <h2 className="centerText">{state.attempt>2 ? 'HINT: ' + hintText(state.word) : ''}</h2> : ''}
+            {state.completed ? <RefreshButton/> : ''}
         </div>
     )
 }
